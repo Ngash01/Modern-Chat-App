31fb4c4c17352fbd2fbbeae4b97db02fcf5e7835
Ngash01 <nganga.amos001@gmail.com>
2024-03-22 10:34:15 +0300

    modified use-chat-query

--- hooks/use-chat-query.ts
+++ hooks/use-chat-query.ts
@@ -35,10 +35,12 @@ export const useChatQuery = ({
 
     const {data, fetchNextPage, hasNextPage, isFetchingNextPage, status} = useInfiniteQuery({
         queryKey: [queryKey],
-        queryFn:fetchMessages, 
-        getNextPageParam:(lastPage) => lastPage?.nextCursor,
-        refetchInterval: isConnected ? false : 1000, 
+        queryFn: fetchMessages, 
+        getNextPageParam: (lastPage) => lastPage?.nextCursor,
+        refetchInterval: isConnected ? false : 1000,
+        initialPageParam: undefined, // Add this line
     })
+    
     return{
         data,
         fetchNextPage,
